7159784faa8edf25b718a59073caa5681f3b7a0f
zhnnnnn <475816908@qq.com>
2016-04-25 10:48:19 +0800

    Initial Commit

---
+++ ZHNautoHideSection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHNautoHideSection
+//
+//  Created by zhn on 16/4/25.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZHNautoHideSection/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZHNautoHideSection
+//
+//  Created by zhn on 16/4/25.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
